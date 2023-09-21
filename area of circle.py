9148c3db676a6259058e03a6566053644a0c9e59
Madhumita Naskar <145662171+voidmadhumita@users.noreply.github.com>
2023-09-21 15:34:23 +0530

    Add files via upload

---
+++ area of circle.py
@@ -0,0 +1,3 @@
+radius=float(input("enter circle radius:"))
+area=3.142*(radius*radius)
+print('%0.2f radious circle area is % 0.2f'%(radius,area))
---
+++ area of square perimetter.py
@@ -0,0 +1,5 @@
+side=float(input("enter square side :"))
+area=side*side
+perimetter=side*4
+print("%0.2f side square area is %0.2f"%(side,area))
+print("%0.2f side square perimetter is %0.2f"%(side,perimetter))
